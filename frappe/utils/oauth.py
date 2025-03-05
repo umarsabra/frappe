bc81e340c784eb1aa9f9bc140229dac9ba8134f4
Gavin D'souza <gavin18d@gmail.com>
2025-03-05 11:25:54 +0100

    fix: Move on_session_creation hook after session is created

--- frappe/utils/oauth.py
+++ frappe/utils/oauth.py
@@ -205,7 +205,6 @@ def login_oauth_user(
 		)
 
 	frappe.local.login_manager.login_as(user)
-	frappe.local.login_manager.run_trigger("on_session_creation")
 
 	# because of a GET request!
 	frappe.db.commit()
