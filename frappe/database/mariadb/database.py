4a04b0a17f66520187ba0f4367988944c644fb74
Akhil Narang <me@akhilnarang.dev>
2025-03-17 16:36:27 +0530

    Merge pull request #31523 from akhilnarang/set-mariadb-collation
    fix(mariadb): set collation in connection

--- frappe/database/mariadb/database.py
+++ frappe/database/mariadb/database.py
@@ -120,6 +120,7 @@ class MariaDBConnectionUtil:
 			"user": self.user,
 			"conv": self.CONVERSION_MAP,
 			"charset": "utf8mb4",
+			"collation": "utf8mb4_unicode_ci",
 			"use_unicode": True,
 		}
 
@@ -319,7 +320,7 @@ class MariaDBDatabase(MariaDBConnectionUtil, MariaDBExceptionUtil, Database):
 			`doctype` VARCHAR(180) NOT NULL,
 			`data` TEXT,
 			UNIQUE(user, doctype)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8"""
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"""
 		)
 
 	@staticmethod
